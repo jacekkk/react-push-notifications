937e51ccb89bafbfc5963896bb68be3b0533ddbe
Jacek Budzynski <jacek.budzynski.92@gmail.com>
2021-09-23 18:19:47 +0100

    Read Firebase credentials from env variable

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const admin = require('firebase-admin')
 const { addMessage } = require('./controllers')
+require('dotenv').config()
 
 const app = express()
 
@@ -11,9 +12,11 @@ app.use(express.static('dist'))
 app.get('/api', (req, res) => res.status(200).json({ status: 'ok' }))
 app.post('/api/messages', addMessage)
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDS)
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server listening on port ${process.env.PORT || 8080}`)
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   })
 })
